553f5016572e7230a4525e62536679cb5a525fca
ehddms122 <ehddms122@naver.com>
2024-05-07 22:18:50 +0900

    check, delete에 word.id가 안먹힘

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,14 +1,47 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+
+export default function Word({ word: w }) {     // {word}로 들어온 변수를 w에 새로 할당
+
+    const [word, setWord] = useState(w);
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone)
 
     function toggleShow() {
         setIsShow(!isShow);
     }
-    function toggleDone(){
-        setIsDone(!isDone);
+
+    function toggleDone() {
+        fetch(`http://localhost:3001/words/${word.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                ...word,
+                isDone: !isDone,
+            }),
+        }).then(res => {            // 비동기로 실행 후, 서버로 응답을 받으면 실행
+            if (res.ok) {
+                setIsDone(!isDone);
+            }
+        });
+    }
+
+    function del() {
+        if (window.confirm("삭제 하시겠습니까?")) {
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: "DELETE",
+            }).then(res =>{
+                if(res.ok){
+                    setWord({id:0})
+                }
+            })
+        }
+    }
+    
+    if(word.id === 0){
+        return null;
     }
 
     return (
@@ -20,9 +53,9 @@ export default function Word({ word }) {
             <td>{isShow && word.kor}</td>
             <td>
                 <button onClick={toggleShow}>{isShow ? "숨기기" : "보기"}</button>
-                <button className="btn_del">삭제</button>
+                <button onClick={del} className="btn_del">삭제</button>
             </td>
         </tr>
 
     )
-} 
+}
